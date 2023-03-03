ecfe7e179292a0e40a1c715be76c38b71c19ecaa
Evhen Diulin <zheka.diulin@gmail.com>
2023-03-03 18:12:59 +0200

    Table orders

--- bkend/db/schema.rb
+++ bkend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_03_160116) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_03_161002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_03_160116) do
     t.string "name"
     t.string "description"
     t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.decimal "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
